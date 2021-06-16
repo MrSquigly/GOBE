fc0a969ec39b23feecde369509231d0a038838c0
Bigred0303 <grantimaxi03@gmail.com>
2021-06-16 18:37:42 -0400

    Button Update
    Go away no one needs to know what I did

--- Ants vs Termites/Assets/Scripts/pauseMenu.cs
+++ Ants vs Termites/Assets/Scripts/pauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class pauseMenu : MonoBehaviour
 {
@@ -21,6 +22,16 @@ public class pauseMenu : MonoBehaviour
                 Pause();
             }
         }
+    }
+
+    public void upgradeMenu()
+    {
+        SceneManager.LoadScene("upgradeMenu");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("mainMenu");
     }
 
     public void Resume()
