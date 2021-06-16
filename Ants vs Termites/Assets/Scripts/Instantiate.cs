b9af4a3ca6dc1459d10825748d5274fcf549c7be
MrSquigly <43219574+MrSquigly@users.noreply.github.com>
2021-06-16 12:02:57 -0400

    Cooldown on spacebar
    Fixed the issue of enemies spawning too fast.
    
    Co-Authored-By: barzie03 <84414028+barzie03@users.noreply.github.com>

--- Ants vs Termites/Assets/Scripts/Instantiate.cs
+++ Ants vs Termites/Assets/Scripts/Instantiate.cs
@@ -7,27 +7,40 @@ public class Instantiate : MonoBehaviour
     
     public GameObject spritePrefab;
 
-    float timer;
     public float cooldown;
+    private bool canPress = true;
+
     void Start()
     {
         Instantiate(spritePrefab, transform.position, Quaternion.identity);
+        StartCoroutine(pressCooldown());
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= cooldown)
+        if (canPress == true)
         {
             if (Input.GetKeyDown("space"))
             {
-                if (PlayerPrefs.GetFloat("Food") > 10)
+                if (PlayerPrefs.GetFloat("Food") >= 10)
                 {
+                    canPress = false;
                     Instantiate(spritePrefab, transform.position, Quaternion.identity);
                     PlayerPrefs.SetFloat("Food", PlayerPrefs.GetFloat("Food") - 10);
                 }
             }
         }
     }
+
+    private IEnumerator pressCooldown()
+    {
+        while (true)
+        {
+            canPress = true;
+            yield return new WaitForSeconds(0.8f); // I used .2 secs but you can update it as fast as you want
+            Debug.Log(canPress);
+        }
+    }
+    
 }
