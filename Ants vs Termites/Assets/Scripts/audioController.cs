e0e33efd8aafae9f36fb5dd694aa2bbfcdab9b82
barzie03 <84414028+barzie03@users.noreply.github.com>
2021-06-17 13:54:12 -0400

    Smart Shuffle
    not going to have the same song twice in a row

--- Ants vs Termites/Assets/Scripts/audioController.cs
+++ Ants vs Termites/Assets/Scripts/audioController.cs
@@ -9,6 +9,7 @@ public class audioController : MonoBehaviour
     public AudioClip tossACoin;
     public AudioSource audio;
     private int x;
+    private int y;
 
     void start()
     {
@@ -20,20 +21,23 @@ public class audioController : MonoBehaviour
         if (audio.isPlaying == false)
         {
             x = Random.Range(1, 4);
-            if (x == 1)
+            if (x == 1 && y != 1)
             {
                 audio.clip = skyrimTheme;
                 audio.Play();
+                y = x;
             }
-            else if (x == 2)
+            else if (x == 2 && y !=2)
             {
                 audio.clip = skyrimTheme2;
                 audio.Play();
+                y = x;
             }
-            else if (x == 3)
+            else if (x == 3 && y != 3)
             {
                 audio.clip = tossACoin;
                 audio.Play();
+                y = x;
             }
     }
     }
